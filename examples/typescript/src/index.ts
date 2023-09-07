139379c16af40c7daff48c2ac3f72d8177a0d393
IoT Manager <67171972+IoTManagerProject@users.noreply.github.com>
2023-09-07 23:22:39 +0200

    прошивка с json

--- examples/typescript/src/index.ts
+++ examples/typescript/src/index.ts
@@ -1,37 +1,39 @@
-const baudrates = document.getElementById("baudrates") as HTMLSelectElement;
-const connectButton = document.getElementById("connectButton") as HTMLButtonElement;
-const disconnectButton = document.getElementById("disconnectButton") as HTMLButtonElement;
-const resetButton = document.getElementById("resetButton") as HTMLButtonElement;
-const consoleStartButton = document.getElementById("consoleStartButton") as HTMLButtonElement;
-const consoleStopButton = document.getElementById("consoleStopButton") as HTMLButtonElement;
-const eraseButton = document.getElementById("eraseButton") as HTMLButtonElement;
-const addFileButton = document.getElementById("addFile") as HTMLButtonElement;
-const programButton = document.getElementById("programButton");
-const filesDiv = document.getElementById("files");
-const terminal = document.getElementById("terminal");
-const programDiv = document.getElementById("program");
-const consoleDiv = document.getElementById("console");
-const lblBaudrate = document.getElementById("lblBaudrate");
-const lblConsoleFor = document.getElementById("lblConsoleFor");
-const lblConnTo = document.getElementById("lblConnTo");
-const table = document.getElementById("fileTable") as HTMLTableElement;
-const alertDiv = document.getElementById("alertDiv");
+const baudrates = document.getElementById('baudrates') as HTMLSelectElement;
+const connectButton = document.getElementById('connectButton') as HTMLButtonElement;
+const disconnectButton = document.getElementById('disconnectButton') as HTMLButtonElement;
+const resetButton = document.getElementById('resetButton') as HTMLButtonElement;
+const consoleStartButton = document.getElementById('consoleStartButton') as HTMLButtonElement;
+const consoleStopButton = document.getElementById('consoleStopButton') as HTMLButtonElement;
+const eraseButton = document.getElementById('eraseButton') as HTMLButtonElement;
+const addFileButton = document.getElementById('addFile') as HTMLButtonElement;
+const programButton = document.getElementById('programButton');
+const filesDiv = document.getElementById('files');
+const terminal = document.getElementById('terminal');
+const programDiv = document.getElementById('program');
+const consoleDiv = document.getElementById('console');
+const lblBaudrate = document.getElementById('lblBaudrate');
+const lblConsoleFor = document.getElementById('lblConsoleFor');
+const lblConnTo = document.getElementById('lblConnTo');
+const table = document.getElementById('fileTable') as HTMLTableElement;
+const alertDiv = document.getElementById('alertDiv');
 
-let firmSettings = [
+const firmSettings = [
   {
-    "addr":"0x00000000",
-    "link":"https://portaller.cloud/compiler/files/rise33@yandex.ru/1694086560920/firmware.bin"
- },
- {
-   "addr":"0x00100000",
-   "link":"https://portaller.cloud/compiler/files/rise33@yandex.ru/1694086560920/littlefs.bin"
- }
+    addr: '0x00000000',
+    link: 'https://portaller.cloud/compiler/files/rise33@yandex.ru/1694120212546/firmware.bin',
+    data: '',
+  },
+  {
+    addr: '0x00100000',
+    link: 'https://portaller.cloud/compiler/files/rise33@yandex.ru/1694120212546/littlefs.bin',
+    data: '',
+  },
 ];
 
 // This is a frontend example of Esptool-JS using local bundle file
 // To optimize use a CDN hosted version like
 // https://unpkg.com/esptool-js@0.2.0/bundle.js
-import { ESPLoader, FlashOptions, LoaderOptions, Transport } from "../../../lib";
+import { ESPLoader, FlashOptions, LoaderOptions, Transport } from '../../../lib';
 
 declare let Terminal; // Terminal is imported in HTML script
 declare let CryptoJS; // CryptoJS is imported in HTML script
@@ -44,10 +46,10 @@ let transport: Transport;
 let chip: string = null;
 let esploader: ESPLoader;
 
-disconnectButton.style.display = "none";
-eraseButton.style.display = "none";
-consoleStopButton.style.display = "none";
-filesDiv.style.display = "none";
+disconnectButton.style.display = 'none';
+eraseButton.style.display = 'none';
+consoleStopButton.style.display = 'none';
+filesDiv.style.display = 'none';
 
 createTableFromJson();
 
@@ -60,29 +62,21 @@ function handleFileSelect(evt) {
 
   reader.onload = (ev: ProgressEvent<FileReader>) => {
     evt.target.data = ev.target.result;
-    console.log("evt.target.data", evt.target.data);
+    console.log('evt.target.data', evt.target.data);
   };
 
   reader.readAsBinaryString(file);
 }
 
-function getFile(url) {
-  console.log("try get file");
-  var xhr = new XMLHttpRequest();
+function getFile(url, index) {
+  const xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
-
-  // Hack to pass bytes through unprocessed.
   xhr.overrideMimeType('text/plain; charset=x-user-defined');
-
-  xhr.onreadystatechange = function(e) {
-  if (this.readyState == 4 && this.status == 200) {
-    var binStr = this.responseText;
-    console.log("---", binStr);
-    //for (var i = 0, len = binStr.length; i < len; ++i) {
-    //  var c = binStr.charCodeAt(i);
-    //  //String.fromCharCode(c & 0xff);
-    //  var byte = c & 0xff;  // byte at offset i
-    //}
+  xhr.onreadystatechange = function (e) {
+    if (this.readyState == 4 && this.status == 200) {
+      const binStr = this.responseText;
+      console.log('get bin file from url', url);
+      firmSettings[index].data = binStr;
     }
   };
 
@@ -124,16 +118,16 @@ connectButton.onclick = async () => {
     term.writeln(`Error: ${e.message}`);
   }
 
-  console.log("Settings done for :" + chip);
-  lblBaudrate.style.display = "none";
-  lblConnTo.innerHTML = "Connected to device: " + chip;
-  lblConnTo.style.display = "block";
-  baudrates.style.display = "none";
-  connectButton.style.display = "none";
-  disconnectButton.style.display = "initial";
-  eraseButton.style.display = "initial";
-  filesDiv.style.display = "initial";
-  consoleDiv.style.display = "none";
+  console.log('Settings done for :' + chip);
+  lblBaudrate.style.display = 'none';
+  lblConnTo.innerHTML = 'Connected to device: ' + chip;
+  lblConnTo.style.display = 'block';
+  baudrates.style.display = 'none';
+  connectButton.style.display = 'none';
+  disconnectButton.style.display = 'initial';
+  eraseButton.style.display = 'initial';
+  filesDiv.style.display = 'initial';
+  consoleDiv.style.display = 'none';
 };
 
 resetButton.onclick = async () => {
@@ -159,92 +153,36 @@ eraseButton.onclick = async () => {
   }
 };
 
-addFileButton.onclick = () => {
-  const rowCount = table.rows.length;
-  const row = table.insertRow(rowCount);
-
-  //Column 1 - Offset
-  const cell1 = row.insertCell(0);
-  const element1 = document.createElement("input");
-  element1.type = "text";
-  element1.id = "offset" + rowCount;
-  element1.value = "0x1000!";
-  cell1.appendChild(element1);
-
-  // Column 2 - File selector
-  const cell2 = row.insertCell(1);
-  const element2 = document.createElement("input");
-  element2.type = "file";
-  element2.id = "selectFile" + rowCount;
-  element2.name = "selected_File" + rowCount;
-  element2.addEventListener("change", handleFileSelect, false);
-  cell2.appendChild(element2);
-
-  // Column 3  - Progress
-  const cell3 = row.insertCell(2);
-  cell3.classList.add("progress-cell");
-  cell3.style.display = "none";
-  cell3.innerHTML = `<progress value="0" max="100"></progress>`;
-
-  // Column 4  - Remove File
-  const cell4 = row.insertCell(3);
-  cell4.classList.add("action-cell");
-  if (rowCount > 1) {
-    const element4 = document.createElement("input");
-    element4.type = "button";
-    const btnName = "button" + rowCount;
-    element4.name = btnName;
-    element4.setAttribute("class", "btn");
-    element4.setAttribute("value", "Remove"); // or element1.value = "button";
-    element4.onclick = function () {
-      removeRow(row);
-    };
-    cell4.appendChild(element4);
-  }
-};
-
-
-
-
-
 function createTableFromJson() {
-  for (var i = 0; i < firmSettings.length; i++) {
+  for (let i = 0; i < firmSettings.length; i++) {
     console.log('------');
     const rowCount = table.rows.length;
     const row = table.insertRow(rowCount);
-  
+
     //Column 1 - Offset
     const cell1 = row.insertCell(0);
-    const element1 = document.createElement("input");
-    element1.type = "text";
-    element1.id = "offset" + rowCount;
-    element1.value = firmSettings[i].addr;
+    const element1 = document.createElement('p');
+    element1.id = 'offset' + rowCount;
+    element1.innerText = firmSettings[i].addr;
     cell1.appendChild(element1);
 
-    getFile(firmSettings[i].link);
+    getFile(firmSettings[i].link, i);
 
-    // Column 2 - File selector
+    // Column 2 - File name
     const cell2 = row.insertCell(1);
-    const element2 = document.createElement("input");
-    element2.type = "file";
-    element2.id = "selectFile" + rowCount;
-    element2.name = "selected_File" + rowCount;
-    element2.addEventListener("change", handleFileSelect, false);
-    //console.log("element2 ", element2);
+    const element2 = document.createElement('p');
+    element2.id = 'parag' + rowCount;
+    let text = firmSettings[i].link;
+    text = text.substring(text.lastIndexOf('/') + 1, text.length);
+    element2.innerText = text;
     cell2.appendChild(element2);
-
 
     // Column 3  - Progress
     const cell3 = row.insertCell(2);
-    cell3.classList.add("progress-cell");
-    cell3.style.display = "none";
+    cell3.classList.add('progress-cell');
+    cell3.style.display = 'none';
     cell3.innerHTML = `<progress value="0" max="100"></progress>`;
   }
-}
-
-function removeRow(row) {
-  const rowIndex = Array.from(table.rows).indexOf(row);
-  table.deleteRow(rowIndex);
 }
 
 // to be called on disconnect - remove any stale references of older connections if any
@@ -258,14 +196,14 @@ disconnectButton.onclick = async () => {
   if (transport) await transport.disconnect();
 
   term.clear();
-  baudrates.style.display = "initial";
-  connectButton.style.display = "initial";
-  disconnectButton.style.display = "none";
-  eraseButton.style.display = "none";
-  lblConnTo.style.display = "none";
-  filesDiv.style.display = "none";
-  alertDiv.style.display = "none";
-  consoleDiv.style.display = "initial";
+  baudrates.style.display = 'initial';
+  connectButton.style.display = 'initial';
+  disconnectButton.style.display = 'none';
+  eraseButton.style.display = 'none';
+  lblConnTo.style.display = 'none';
+  filesDiv.style.display = 'none';
+  alertDiv.style.display = 'none';
+  consoleDiv.style.display = 'initial';
   cleanUp();
 };
 
@@ -275,23 +213,23 @@ consoleStartButton.onclick = async () => {
     device = await navigator.serial.requestPort({});
     transport = new Transport(device);
   }
-  lblConsoleFor.style.display = "block";
-  consoleStartButton.style.display = "none";
-  consoleStopButton.style.display = "initial";
-  programDiv.style.display = "none";
+  lblConsoleFor.style.display = 'block';
+  consoleStartButton.style.display = 'none';
+  consoleStopButton.style.display = 'initial';
+  programDiv.style.display = 'none';
 
   await transport.connect();
   isConsoleClosed = false;
 
   while (true && !isConsoleClosed) {
     const val = await transport.rawRead();
-    if (typeof val !== "undefined") {
+    if (typeof val !== 'undefined') {
       term.write(val);
     } else {
       break;
     }
   }
-  console.log("quitting console");
+  console.log('quitting console');
 };
 
 consoleStopButton.onclick = async () => {
@@ -299,52 +237,12 @@ consoleStopButton.onclick = async () => {
   await transport.disconnect();
   await transport.waitForUnlock(1500);
   term.clear();
-  consoleStartButton.style.display = "initial";
-  consoleStopButton.style.display = "none";
-  programDiv.style.display = "initial";
+  consoleStartButton.style.display = 'initial';
+  consoleStopButton.style.display = 'none';
+  programDiv.style.display = 'initial';
 };
 
-function validate_program_inputs() {
-  const offsetArr = [];
-  const rowCount = table.rows.length;
-  let row;
-  let offset = 0;
-  let fileData = null;
-
-  // check for mandatory fields
-  for (let index = 1; index < rowCount; index++) {
-    row = table.rows[index];
-
-    //offset fields checks
-    const offSetObj = row.cells[0].childNodes[0];
-    offset = parseInt(offSetObj.value);
-
-    // Non-numeric or blank offset
-    if (Number.isNaN(offset)) return "Offset field in row " + index + " is not a valid address!";
-    // Repeated offset used
-    else if (offsetArr.includes(offset)) return "Offset field in row " + index + " is already in use!";
-    else offsetArr.push(offset);
-
-    const fileObj = row.cells[1].childNodes[0];
-    fileData = fileObj.data;
-    if (fileData == null) return "No file selected for row " + index + "!";
-  }
-  return "success";
-}
-
 programButton.onclick = async () => {
-  const alertMsg = document.getElementById("alertmsg");
-  const err = validate_program_inputs();
-
-  if (err != "success") {
-    alertMsg.innerHTML = "<strong>" + err + "</strong>";
-    alertDiv.style.display = "block";
-    return;
-  }
-
-  // Hide error message
-  alertDiv.style.display = "none";
-
   const fileArray = [];
   const progressBars = [];
 
@@ -354,22 +252,19 @@ programButton.onclick = async () => {
     const offSetObj = row.cells[0].childNodes[0] as HTMLInputElement;
     const offset = parseInt(offSetObj.value);
 
-    const fileObj = row.cells[1].childNodes[0] as ChildNode & { data: string };
     const progressBar = row.cells[2].childNodes[0];
 
-    progressBar.textContent = "0";
+    progressBar.textContent = '0';
     progressBars.push(progressBar);
 
-    row.cells[2].style.display = "initial";
-    //row.cells[3].style.display = "none";
-
-    fileArray.push({ data: fileObj.data, address: offset });
+    row.cells[2].style.display = 'initial';
+    fileArray.push({ data: firmSettings[index - 1].data, address: offset });
   }
 
   try {
     const flashOptions: FlashOptions = {
       fileArray: fileArray,
-      flashSize: "keep",
+      flashSize: 'keep',
       eraseAll: false,
       compress: true,
       reportProgress: (fileIndex, written, total) => {
@@ -384,10 +279,7 @@ programButton.onclick = async () => {
   } finally {
     // Hide progress bars and show erase buttons
     for (let index = 1; index < table.rows.length; index++) {
-      table.rows[index].cells[2].style.display = "none";
-      //table.rows[index].cells[3].style.display = "initial";
+      table.rows[index].cells[2].style.display = 'none';
     }
   }
 };
-
-//addFileButton.onclick(this);
